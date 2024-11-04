490631b51014ca76cf9e349bd44c9130ba54ca07
Thanh <thanh.nb2311@gmail.com>
2024-11-04 23:31:01 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,11 +4,23 @@
 // fragment - thay cho việc bọc div cha
 
 import './style.css';
+
 const MyComponent = () => {
+    const thanhReact = "Thanh";
+    // const thanhReact = 25; number
+    // const thanhReact = true; boolean
+    // const thanhReact = underfined;
+    // const thanhReact = null;
+    // const thanhReact = [1, 2, 3]; array
+    // const thanhReact = {
+    //     name:"thanh",
+    //     age: 25
+    // }
     return (
         <>
-            <div>THÀNH đang học ReactJS</div>
-            <div className='child'>child</div>
+            <div>{JSON.stringify(thanhReact)} đang học ReactJS</div>
+            <div>{console.log("THANH")}</div>
+            <div className='child' style={{ borderRadius: "10px"}}>child</div>
         </>
     );
   };
