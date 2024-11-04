6c92ddf2bcc8af96e62cd0c4dccd68513d5399d3
Thanh <thanh.nb2311@gmail.com>
2024-11-04 22:07:24 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,15 @@
 // () => { }
 // component = html + css + js
+// JSX - Dùng cho việc code JS trong HTML
+// fragment - thay cho việc bọc div cha
 
+import './style.css';
 const MyComponent = () => {
     return (
-      <div>THÀNH đang học ReactJS</div>
+        <>
+            <div>THÀNH đang học ReactJS</div>
+            <div className='child'>child</div>
+        </>
     );
   };
   
