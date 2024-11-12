1b497c111dd2f1da3ebecc543a8cf15867350727
Thanh <thanh.nb2311@gmail.com>
2024-11-13 01:42:11 +0700

    Video 33

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,26 +3,28 @@
 // JSX - Dùng cho việc code JS trong HTML
 // fragment - thay cho việc bọc div cha
 
-import './style.css';
+import "./style.css";
 
 const MyComponent = () => {
-    const thanhReact = "Thanh";
-    // const thanhReact = 25; number
-    // const thanhReact = true; boolean
-    // const thanhReact = underfined;
-    // const thanhReact = null;
-    // const thanhReact = [1, 2, 3]; array
-    // const thanhReact = {
-    //     name:"thanh",
-    //     age: 25
-    // }
-    return (
-        <>
-            <div>{JSON.stringify(thanhReact)} đang học ReactJS</div>
-            <div>{console.log("THANH")}</div>
-            <div className='child' style={{ borderRadius: "10px"}}>child</div>
-        </>
-    );
-  };
-  
-  export default MyComponent;
+  const thanhReact = "Thanh";
+  // const thanhReact = 25; number
+  // const thanhReact = true; boolean
+  // const thanhReact = underfined;
+  // const thanhReact = null;
+  // const thanhReact = [1, 2, 3]; array
+  // const thanhReact = {
+  //     name:"thanh",
+  //     age: 25
+  // }
+  return (
+    <>
+      <div>{JSON.stringify(thanhReact)} đang học ReactJS</div>
+      <div>{console.log("THANH")}</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
+};
+
+export default MyComponent;
---
+++ src/components/todo/TodoNew.jsx
@@ -0,0 +1,10 @@
+const TodoNew = () => {
+  return (
+    <div className="todo-new">
+      <input type="text" placeholder="Enter yout task" />
+      <button>Add</button>
+    </div>
+  );
+};
+
+export default TodoNew;
