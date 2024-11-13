a265177f64bc5dc8dba31a90775d05b1aed00d16
Thanh <thanh.nb2311@gmail.com>
2024-11-13 11:36:26 +0700

    video 36

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import TodoNew from "./components/todo/TodoNew";
 import reactLogo from "./assets/react.svg";
 
 const App = () => {
+  const thanhit = "Thành Thành";
+  const age = 25;
+  const data = {
+    address: "hanoi",
+    country: "vietnam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData 
+        name={thanhit}
+        age={age}
+        data={data}
+      />
       <div className="todo-image">
         <img src={reactLogo} className="logo" />
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,8 +1,26 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    // props là một object {}
+    // {
+    //     name: "Thành"
+    //     age: 25;
+    //     data: {}
+    // }
+    // CÁCH CODE 1
+    const {name, age, data} = props;
+    // các biến khai báo trong const chính là tên các object
+    // được khai báo trong component
+    // CÁCH CODE 2
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+    // CÁCH CODE 3 (KHÔNG NÊN DÙNG VỚI NGƯỜI MỚI)
+
+    console.log(">>> check props: ", props)
   return (
     <div className="todo-data">
-      <div>Learning React</div>
-      <div>Watching Youtube</div>
+        <div>My name is {name} </div>
+        <div>Learning React</div>
+        <div>Watching Youtube</div>
     </div>
   );
 };
